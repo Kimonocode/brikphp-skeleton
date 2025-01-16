ed78dea90d11bfacdfece1d9d783f9d25e1711ae
= <=>
2025-01-16 16:03:46 +0100

    modification de la console

--- start/routes.php
+++ start/routes.php
@@ -7,5 +7,7 @@ use GuzzleHttp\Psr7\Response;
 $router = Kernel::container()->get(RouterInterface::class);
 
 $router->get('home', '/', function () {
-    return new Response(200, ['Content-Type: text/html'], "<h1>Welcome To BrikPhp<h1/>");
+    return new Response(200, ['Content-Type: application/json'], json_encode([
+        "message" => "Hello World !"
+    ], JSON_PRETTY_PRINT));
 });
