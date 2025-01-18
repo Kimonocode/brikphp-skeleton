c1ef714d22c90f74d23c541941747c4fb382ad33
= <=>
2025-01-18 20:44:17 +0100

    modif

--- start/routes.php
+++ start/routes.php
@@ -1,13 +1,7 @@
 <?php
 
-use Brikphp\Core\Kernel;
-use Brikphp\Core\Router\RouterInterface;
-use GuzzleHttp\Psr7\Response;
-
-$router = Kernel::container()->get(RouterInterface::class);
-
 $router->get('home', '/', function () {
-    return new Response(
+    return new Brikphp\Http\Response(
         200, 
         ['Content-Type' => 'application/json'], 
         json_encode(["message" => "hello World"], JSON_UNESCAPED_UNICODE)
