b25340896f99382192c827c45f8dba0433f5ed22
= <=>
2025-01-19 02:02:53 +0100

    changement index.php

--- public/index.php
+++ public/index.php
@@ -1,23 +1,21 @@
 <?php
 
 use Brikphp\Core\App;
-use Brikphp\Http\Response;
 
 require dirname(__DIR__) . '/vendor/autoload.php';
 
 $app = new App();
 
 try {
+
+    $request  = $app->fromGlobals();
+    $response = $app->run($request);
     
-    $response = $app->run($app->fromGlobals());
     $app->emit($response);
 
 } catch (Throwable $e) {
-    $app->emit( new Response(
-        500,
-        ['Content-Type' => 'text/plain'],
-        'Une erreur interne est survenue: ' . $e->getMessage()
-    ));
+
+    $app->fail($e);
 }
 
 
